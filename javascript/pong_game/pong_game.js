6684939108ce6037b526d6dbe63530480ac0d079
Evan Hughes <evanhughes3@gmail.com>
2015-04-27 10:18:50 -0700

    drew net in the middle

--- javascript/pong_game/pong_game.js
+++ javascript/pong_game/pong_game.js
@@ -1,87 +1,105 @@
 "use strict;"
 
-  // Game board
-  var WIDTH=700, HEIGHT=600, pi=Math.PI;
-  // Gamestate
-  var canvas, ctx, keystate;
-  // Logic
-  var player, ai, ball;
+// Game board
+var WIDTH=700, HEIGHT=600, pi=Math.PI;
+// Gamestate
+var canvas, ctx, keystate;
+// Logic
+var player, ai, ball;
 
-  player = {
-    x: null,
-    y: null,
-    width: 20,
-    height: 100,
+player = {
+  x: null,
+  y: null,
+  width: 20,
+  height: 100,
 
-    update: function() {},
-    draw: function() {
-      ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-  };
+  update: function() {},
+  draw: function() {
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+};
 
-  ai = {
-    x: null,
-    y: null,
-    width: 20,
-    height: 100,
+ai = {
+  x: null,
+  y: null,
+  width: 20,
+  height: 100,
 
-    update: function() {},
-    draw: function() {
-      ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-  };
+  update: function() {},
+  draw: function() {
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+};
 
-  ball = {
-    x: null,
-    y: null,
-    side: 20,
+ball = {
+  x: null,
+  y: null,
+  side: 20,
 
-    update: function() {},
-    draw: function() {
-      ctx.fillRect(this.x, this.y, this.side, this.side);
-    }
-  };
+  update: function() {},
+  draw: function() {
+    ctx.fillRect(this.x, this.y, this.side, this.side);
+  }
+};
 
 
-  function main () {
-    canvas = document.createElement("canvas");
-    canvas.width = WIDTH;
-    canvas.height = HEIGHT;
-    ctx = canvas.getContext("2d");
-    document.body.appendChild(canvas);
+function main () {
+  canvas = document.createElement("canvas");
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
+  ctx = canvas.getContext("2d");
+  document.body.appendChild(canvas);
 
-    init();
+  init();
 
-    var loop = function() {
-      update();
-      draw();
+  var loop = function() {
+    update();
+    draw();
 
-      window.requestAnimationFrame(loop, canvas);
-    };
     window.requestAnimationFrame(loop, canvas);
+  };
+  window.requestAnimationFrame(loop, canvas);
+}
+
+function init() {
+  player.x = player.width;
+  player.y = (HEIGHT - player.height)/2;
+
+  ai.x = WIDTH - (player.width + ai.width);
+  ai.y = (HEIGHT - ai.height)/2;
+
+  ball.x = (WIDTH - ball.side)/2;
+  ball.y = (HEIGHT - ball.side)/2;
+}
+
+function update() {
+  ball.update();
+  player.update();
+  ai.update();
+}
+
+function draw() {
+  ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+  ctx.save();
+  ctx.fillStyle = "#fff";
+
+  ball.draw();
+  player.draw();
+  ai.draw();
+
+  // draw the net in the middle
+  var w = 4;
+  var x = (WIDTH - w)*0.5;
+  var y = 0;
+  var step = HEIGHT/25
+  while ( y < HEIGHT) {
+    ctx.fillRect(x, y+step*0.25, w, step*0.5);
+    y += step;
   }
 
-  function init() {
-    player.x = player.width;
-    player.y = (HEIGHT - player.height)/2;
 
-    ai.x = WIDTH - (player.width + ai.width);
-    ai.y = (HEIGHT - ai.height)/2;
+  ctx.restore();
+}
 
-    ball.x = (WIDTH - ball.side)/2;
-    ball.y = (HEIGHT - ball.side)/2;
-  }
-
-  function update() {
-    ball.update();
-    player.update();
-    ai.update();
-  }
-
-  function draw() {
-    ball.draw();
-    player.draw();
-    ai.draw();
-  }
-
-  main();
+main();
