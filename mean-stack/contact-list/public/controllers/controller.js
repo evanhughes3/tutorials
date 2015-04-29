83a4e25db590f7665117f1efcc257343eccf7861
Evan Hughes <evanhughes3@gmail.com>
2015-04-29 12:31:02 -0700

    moved dummy data from controller to server

--- mean-stack/contact-list/public/controllers/controller.js
+++ mean-stack/contact-list/public/controllers/controller.js
@@ -1,5 +1,10 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-  console.log("Hello World from the controller");
+
+  $http.get('/contactList').success(function(response) {
+    console.log("I got the data I requested");
+    $scope.contactList = response;
+  });
+
 }]);
--- mean-stack/contact-list/server.js
+++ mean-stack/contact-list/server.js
@@ -3,5 +3,31 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contactList', function(request, response){
+  console.log("I receeived a GET request");
+
+    person1 = {
+      name: 'Evan',
+      email: 'Evan@evan.com',
+      number: '111-111-1111'
+    }
+
+    person2 = {
+      name: 'Grace',
+      email: 'grace@grace.com',
+      number: '222-222-2222'
+    }
+
+    person3 = {
+      name: 'John',
+      email: 'john@john.com',
+      number: '333-333-3333'
+    }
+
+    var contactList = [person1, person2, person3]
+
+    response.json(contactList);
+  });
+
 app.listen(3000);
 console.log("Server running on port 3000")
