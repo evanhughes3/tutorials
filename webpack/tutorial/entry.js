0286335b083ea828b22711cc3e64b5f024014d0e
Evan Hughes <evanhughes3@gmail.com>
2015-05-25 14:14:36 -0700

    finished webpack tutorial

---
+++ webpack/tutorial/entry.js
@@ -0,0 +1,3 @@
+require("./style.css");
+document.write(require('./content.js'));
+document.write(require('./moreContent.js'));
---
+++ webpack/tutorial/moreContent.js
@@ -0,0 +1,1 @@
+module.exports =  "It also works in development mode"
